ed533077f56e18f66fa47bf61da9818b447979af
ashiqsayyad <ashiqsayyad>
2020-05-26 11:27:56 +0530

    Added new skeleton app for contactless

---
+++ contactlessclientapp/src/main/java/com/ashiqsayyad/contactlessclientapp/ClientApplication.kt
@@ -0,0 +1,9 @@
+package com.ashiqsayyad.contactlessclientapp
+
+import android.app.Application
+
+class ClientApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
